34272ded2f93ffef36d977cfd0d255fceb5d2cd3
Hemanth1310 <hemanth13k@gmail.com>
2026-07-24 13:09:11 +0200

    frontend-context-layout

---
+++ frontend/src/components/layouts/AuthLayout.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import Modal from './Modal'
+import Login from './Login'
+import Register from './Register'
+import type { toggeler } from '../../types'
+type Props = {
+    isOpen: boolean,
+    onClose:()=>void
+}
+
+
+
+const AuthLayout = ({isOpen, onClose}: Props) => {
+
+   const [toggle,setToggle] = useState<toggeler>('login')
+   
+   const title = toggle==='login'?'Login':'Register'
+   const ActiveComponent = toggle==='login'? Login:Register
+
+   const toggleTo =(val:toggeler)=>{
+        setToggle(val)
+   }
+  return (
+    <Modal onClose={onClose} isOpen={isOpen} title={title}>
+        <ActiveComponent toggleTo={toggleTo} onClose={onClose}/>
+    </Modal>
+  )
+}
+
+export default AuthLayout
