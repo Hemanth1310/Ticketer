53bec086a7de2da4446f824554d39153ab5b65f8
Hemanth1310 <hemanth13k@gmail.com>
2026-07-27 12:13:04 +0200

    frontend-header-css-fix

--- frontend/src/components/layouts/Header.tsx
+++ frontend/src/components/layouts/Header.tsx
@@ -21,7 +21,8 @@ const Header = () => {
   }
 
   return (
-    <div className="flex-1 w-screen h-16  border-b-gray-300 border-b-2 flex items-center justify-between fixed top-0 left-0 z-100 pr-5 pl-5 font-mono ">
+    <div className="flex-1 w-screen h-16  border-b-gray-300 border-b-2 flex items-center justify-center fixed top-0 left-0 z-100 pr-5 pl-5 font-mono ">
+      <div className="container flex items-center justify-between">
        <div className='flex sm:hidden' onClick={()=>setIsMenuOpen(prev=>!prev)}>
             <Menu />
         </div>
@@ -70,6 +71,7 @@ const Header = () => {
       isOpen={isModalOpen}
       onClose={closeModal}
       />
+      </div>
     </div>
   )
 }
