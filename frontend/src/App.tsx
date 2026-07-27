3a5c4b975fd2d37e7d146781553897abf612c987
Hemanth1310 <hemanth13k@gmail.com>
2026-07-27 13:19:22 +0200

    frontend-css-fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,12 +11,13 @@ function App() {
   <BrowserRouter>
     <div className='w-screen min-h-screen flex flex-col text-mist-900 bg-mist-50'>
       <Header/>
-      <div className="mt-16 p-5 flex-1 w-full h-full flex gap-5 bg-mist-50 rounded-2xl">
-          <div className="min-h-full w-full rounded-2xl p-5 box-border">
-            <Routes>
-              <Route path="/" element={<Home/>}></Route>
-             
-            </Routes>
+      <div className="mt-16 flex-1 w-full h-full flex gap-5 bg-mist-50 rounded-2xl justify-center">
+          <div className="min-h-full w-full rounded-2xl box-border container flex pt-5 pb-5">
+              <Routes>
+                <Route path="/" element={<Home/>}></Route>
+              
+              </Routes>
+
           </div> 
         </div>
       <ToastContainer/>
