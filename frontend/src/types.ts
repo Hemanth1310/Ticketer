b74b03bc4880bf114c94b6be59fbae66f0e44a70
Hemanth1310 <hemanth13k@gmail.com>
2026-07-27 12:12:51 +0200

    frontend-type-def-movies

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,1 +1,13 @@
 export type toggeler = "login"|"register"
+
+export type Genre = "COMEDY" | "THRILLER" | "DRAMA" | "HORROR" | "ANIMATED" | "ACTION" | "OTHERS"
+
+export type Movies = {
+    id: string;
+    imagePath: string;
+    title: string;
+    description: string | null;
+    genre: Genre;
+    duration: number;
+    featured: boolean;
+}
