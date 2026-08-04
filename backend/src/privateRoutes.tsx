5914d606293a3b96be07c2c0ce544c0b230c5984
Hemanth1310 <hemanth13k@gmail.com>
2026-08-04 16:29:04 +0200

    backend-screen-seat-details

--- backend/src/privateRoutes.tsx
+++ backend/src/privateRoutes.tsx
@@ -1,5 +1,6 @@
 import express from 'express'
 import { prisma } from './prisma.js'
+import { PrismaClientKnownRequestError } from '../generated/prisma/internal/prismaNamespace.js'
 
 const router = express.Router()
 
@@ -37,5 +38,63 @@ router.get("/logout", (req, res) => {
 
   res.status(200).json({ message: "Successfully Logged out" });
 });
+
+router.get("/showtimes/:id",async(req,res)=>{
+    const {id} = req.params
+
+    if(!id){
+        return res.status(500).json({error:"Bad request: invalid id"})
+    }
+
+    try{    
+        const showtimes = await prisma.showtime.findUnique({
+            where:{id},
+            include:{
+                screen:{
+                    include:{
+                        seats:true,
+                    
+                    }
+                }
+            }
+        })
+
+        const seatBookings = await prisma.booking.findMany({
+            select:{
+               seatId:true
+            },
+             where:{
+                showtimeId:id
+             }
+        })
+
+        const seatLocks = await prisma.seatLock.findMany({
+            where:{
+                showtimeId:id
+            },select:{
+                seatId:true
+            }
+        })
+
+        if(!showtimes){
+            return res.status(404).json({error:'no shows found'})
+        }
+
+        return res.status(200).json({
+            payload:{
+                showtimes,
+                seatBookings,
+                seatLocks
+            }
+        })
+    }catch(err){
+        if(err instanceof PrismaClientKnownRequestError){
+                          if(err.code==='P2025'){
+                              return res.status(401).json({error:'Show not found'})
+                          }
+                      }
+                return res.status(500).json({error:'Unexpected error occurred'})
+    }
+})
 
 export default router
