595730d7af72fa526e33c933e5906f3811bceab7
Hemanth1310 <hemanth13k@gmail.com>
2026-08-20 15:32:48 +0200

    backend-booking-confirm-route

--- backend/src/privateRoutes.tsx
+++ backend/src/privateRoutes.tsx
@@ -1,16 +1,16 @@
-import express from 'express'
-import { prisma } from './prisma.js'
-import { PrismaClientKnownRequestError } from '../generated/prisma/internal/prismaNamespace.js'
+import express from "express";
+import { prisma } from "./prisma.js";
+import { PrismaClientKnownRequestError } from "../generated/prisma/internal/prismaNamespace.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/user-details',async(req,res)=>{
-    const userEmail = req.userData?.email
+router.get("/user-details", async (req, res) => {
+  const userEmail = req.userData?.email;
 
-    if(!userEmail){
-       return res.status(403).json({error:'Invalid token'})
-    }
-try {
+  if (!userEmail) {
+    return res.status(403).json({ error: "Invalid token" });
+  }
+  try {
     const user = await prisma.user.findFirst({
       where: {
         email: userEmail,
@@ -30,7 +30,7 @@ try {
   } catch (err) {
     return res.status(403).json({ error: "User not found." });
   }
-})
+});
 
 router.get("/logout", (req, res) => {
   res.clearCookie("token");
@@ -40,279 +40,364 @@ router.get("/logout", (req, res) => {
 });
 
 const SEAT_TYPE_MULTIPLIERS: Record<string, number> = {
-  SILVER: 1.0,    // Base price
-  GOLD: 1.25,     // 25% markup
-  PLATINUM: 1.6,  // 60% markup
+  SILVER: 1.0, // Base price
+  GOLD: 1.25, // 25% markup
+  PLATINUM: 1.6, // 60% markup
 };
 
-router.get("/showtimes/:id",async(req,res)=>{
-    const {id} = req.params
+router.get("/showtimes/:id", async (req, res) => {
+  const { id } = req.params;
 
-    if(!id){
-        return res.status(500).json({error:"Bad request: invalid id"})
+  if (!id) {
+    return res.status(500).json({ error: "Bad request: invalid id" });
+  }
+
+  try {
+    const showtimes = await prisma.showtime.findUnique({
+      where: { id },
+      include: {
+        screen: {
+          include: {
+            seats: true,
+          },
+        },
+      },
+    });
+    if (!showtimes) {
+      return res.status(404).json({ error: "no shows found" });
     }
 
-    try{    
-        const showtimes = await prisma.showtime.findUnique({
-            where:{id},
-            include:{
-                screen:{
-                    include:{
-                        seats:true,
-                    
-                    }
-                }
-            }
-        })
-        if(!showtimes){
-            return res.status(404).json({error:'no shows found'})
-        }
+    const basePrice = Number(showtimes.basePrice || 10.0);
+    const seatsWithPrices = showtimes.screen.seats.map((seat) => {
+      const multiplier = SEAT_TYPE_MULTIPLIERS[seat.type] || 1.0;
+      const calculatedPrice = Number((basePrice * multiplier).toFixed(2));
 
-        const basePrice = Number(showtimes.basePrice || 10.00);
-        const seatsWithPrices = showtimes.screen.seats.map((seat)=>{
-            const multiplier = SEAT_TYPE_MULTIPLIERS[seat.type] || 1.0
-            const calculatedPrice = Number((basePrice*multiplier).toFixed(2))
+      return {
+        ...seat,
+        price: calculatedPrice,
+      };
+    });
 
-            return {
-                ...seat,
-                price:calculatedPrice
-            }
-        })
+    const seatBookings = await prisma.booking.findMany({
+      select: {
+        seats: true,
+      },
+      where: {
+        showtimeId: id,
+        status: "CONFIRMED",
+      },
+    });
 
-        const seatBookings = await prisma.booking.findMany({
-            select:{
-               seats:true
+    const arrOfSeatBookings = seatBookings.flatMap((booking) =>
+      booking.seats.map((s) => s.id),
+    );
+
+    const seatLocks = await prisma.seatLock.findMany({
+      where: {
+        showtimeId: id,
+        expiresAt: { gt: new Date() },
+      },
+      select: {
+        seatId: true,
+      },
+    });
+
+    const arrOfSeatLocks = seatLocks.flatMap((lock) => lock.seatId);
+
+    return res.status(200).json({
+      payload: {
+        showtimes: {
+          ...showtimes,
+          screen: {
+            ...showtimes.screen,
+            seats: seatsWithPrices,
+          },
+        },
+        arrOfSeatBookings,
+        arrOfSeatLocks,
+      },
+    });
+  } catch (err) {
+    if (err instanceof PrismaClientKnownRequestError) {
+      if (err.code === "P2025") {
+        return res.status(401).json({ error: "Show not found" });
+      }
+    }
+    return res.status(500).json({ error: "Unexpected error occurred" });
+  }
+});
+
+router.post("/seatLock/:showtimeId/:seatId", async (req, res) => {
+  const userId = req.userData?.id;
+  const { showtimeId, seatId } = req.params;
+
+  if (!userId || !showtimeId || !seatId) {
+    return res.status(405).json({ error: "Invalid Request." });
+  }
+
+  const FIVE_MINS_MS = 5 * 60 * 1000;
+  const expireAt = Date.now() + FIVE_MINS_MS;
+  try {
+    const existingBooking = await prisma.booking.findMany({
+      where: {
+        showtimeId,
+        status: "CONFIRMED",
+      },
+      select: {
+        seats: true,
+      },
+    });
+
+    const arrOfBookedSeats = existingBooking.flatMap((booking) =>
+      booking.seats.map((s) => s.id),
+    );
+
+    if (arrOfBookedSeats.includes(seatId)) {
+      return res.status(409).json({ error: "Seat is already booked." });
+    }
+
+    const activeLock = await prisma.seatLock.findFirst({
+      where: {
+        seatId,
+        showtimeId,
+        expiresAt: { gt: new Date() },
+      },
+    });
+    if (activeLock) {
+      if (activeLock.userId === userId) {
+        await prisma.seatLock.update({
+          where: {
+            id: activeLock.id,
+          },
+          data: {
+            expiresAt: new Date(expireAt),
+          },
+        });
+        return res
+          .status(200)
+          .json({ message: "Extended time limit for a perticular user." });
+      } else {
+        return res.status(409).json({ error: "Seat on hold by other user" });
+      }
+    }
+
+    const newLock = await prisma.seatLock.create({
+      data: {
+        seatId,
+        showtimeId,
+        expiresAt: new Date(expireAt),
+        userId,
+      },
+    });
+
+    res.status(201).json({ message: "Seat upheld", payload: newLock });
+  } catch (err) {
+    console.error("Seat locking error:", err);
+    return res.status(500).json({ error: "Failed to lock seat" });
+  }
+});
+
+router.delete("/seatLockDelate/:showtimeId/:seatId", async (req, res) => {
+  const userId = req.userData?.id;
+  const { showtimeId, seatId } = req.params;
+
+  if (!userId || !showtimeId || !seatId) {
+    return res.status(405).json({ error: "Invalid Request." });
+  }
+
+  try {
+    await prisma.seatLock.deleteMany({
+      where: {
+        showtimeId,
+        seatId,
+        userId,
+      },
+    });
+    return res.status(200).json({ message: "Seat unlocked successfully" });
+  } catch (error) {
+    return res.status(500).json({ error: "Failed to unlock seat" });
+  }
+});
+
+router.delete("/seatLock/cleanup", async (req, res) => {
+  const userId = req.userData?.id;
+
+  if (!userId) {
+    return res.status(405).json({ error: "Invalid Request." });
+  }
+
+  try {
+    await prisma.seatLock.deleteMany({
+      where: {
+        userId,
+      },
+    });
+    return res.status(200).json({ message: "Seat unlocked successfully" });
+  } catch (error) {
+    return res.status(500).json({ error: "Failed to unlock seat" });
+  }
+});
+
+router.post("/reserveBooking/:showtimeId", async (req, res) => {
+  const { seatIds, ticketPrice, convenienceFee, totalAmount } = req.body;
+  const userId = req.userData?.id;
+  const showtimeId = req.params.showtimeId;
+
+  if (
+    !seatIds ||
+    !ticketPrice ||
+    !convenienceFee ||
+    !totalAmount ||
+    !userId ||
+    !showtimeId
+  ) {
+    return res.status(405).json({ error: "Invalid Request." });
+  }
+
+  try {
+    const booking = await prisma.booking.create({
+      data: {
+        userId,
+        ticketPrice,
+        convenienceFee,
+        totalAmount,
+        showtimeId,
+        seats: {
+          connect: seatIds.map((id: string) => ({ id })),
+        },
+      },
+      include: {
+        seats: true,
+      },
+    });
+
+    if (!booking) {
+      return res.status(409).json({ error: "Booking not possible" });
+    }
+
+    return res.json({
+      message: "Booking Initiated",
+      payload: { bookingId: booking.id },
+    });
+  } catch (err) {
+    console.error("Booking error:", err);
+    return res.status(500).json({ error: "Failed to lock hold booking" });
+  }
+});
+
+router.get("/bookingDetails/:bookingId", async (req, res) => {
+  const { bookingId } = req.params;
+
+  if (!bookingId) {
+    return res.status(405).json({ error: "Invalid Request." });
+  }
+
+  try {
+    const bookingDetails = await prisma.booking.findUnique({
+      where: {
+        id: bookingId,
+      },
+      include: {
+        seats: true,
+        showtime: {
+          include: {
+            movie: true,
+            screen: {
+              include: {
+                theater: true,
+              },
             },
-             where:{
-                showtimeId:id,
-                status:'CONFIRMED'
-             }
-        })
-
-        const arrOfSeatBookings = seatBookings.flatMap(booking=>booking.seats.map(s=>s.id))
-
-
-        const seatLocks = await prisma.seatLock.findMany({
-            where:{
-                showtimeId:id,
-                expiresAt: { gt: new Date() }
-            },select:{
-                seatId:true
-            }
-        })
-
-        const arrOfSeatLocks = seatLocks.flatMap(lock=>lock.seatId)
-
-        return res.status(200).json({
-            payload:{
-                showtimes:{
-                    ...showtimes,
-                    screen:{
-                        ...showtimes.screen,
-                        seats:seatsWithPrices
-                    }
-                },
-                arrOfSeatBookings,
-                arrOfSeatLocks
-            }
-        })
-    }catch(err){
-        if(err instanceof PrismaClientKnownRequestError){
-                          if(err.code==='P2025'){
-                              return res.status(401).json({error:'Show not found'})
-                          }
-                      }
-                return res.status(500).json({error:'Unexpected error occurred'})
+          },
+        },
+      },
+    });
+    if (!bookingDetails) {
+      return res.status(409).json({ error: "Booking Details not found" });
     }
-})
+    console.log(bookingDetails);
+    const { showtime, seats, ...rest } = bookingDetails;
+    const { screen, movie, ...restShowtime } = showtime;
+    const { theater, ...restOfScreen } = screen;
 
-router.post(('/seatLock/:showtimeId/:seatId'),async(req,res)=>{
-    const userId = req.userData?.id
-    const {showtimeId, seatId} = req.params
+    const modifiedBookingDetails = {
+      ...rest,
+      seats,
+      location: showtime.screen.theater.location,
+      theater: showtime.screen.theater.name,
+      showtime: { ...restShowtime },
+      movie,
+      screen: restOfScreen.name,
+    };
+    return res.json({
+      payload: {
+        bookingDetails: modifiedBookingDetails,
+      },
+    });
+  } catch (err) {
+    console.error("Booking not found:", err);
+    return res.status(500).json({ error: "Failed to find booking" });
+  }
+});
 
-    if(!userId || !showtimeId || !seatId){
-        return res.status(405).json({error:"Invalid Request."})
+router.patch("/confirmBooking/:bookingId", async (req, res) => {
+  const { bookingId } = req.params;
+  const userId = req.userData?.id;
+
+  if (!userId || !bookingId) {
+    return res.status(405).json({ error: "Invalid Request." });
+  }
+
+  try {
+    const result = await prisma.$transaction(async (tx) => {
+      const existingBooking = await tx.booking.findFirst({
+        where: { id: bookingId },
+        include: { seats: true },
+      });
+
+      if (!existingBooking) {
+        throw new Error("BOOKING_NOT_FOUND");
+      }
+
+      if (existingBooking.userId !== userId) {
+        throw new Error("UNAUTHORIZED_ACCESS");
+      }
+
+      if (existingBooking.status === "CONFIRMED") {
+        throw new Error("ALREADY_CONFIRMED");
+      }
+
+      const confirmedBooking = await tx.booking.update({
+        where: { id: bookingId },
+        data: { status: "CONFIRMED" },
+      });
+      const seatIds = existingBooking.seats.map((seat) => seat.id);
+      await tx.seatLock.deleteMany({
+        where: {
+          showtimeId: existingBooking.showtimeId,
+          seatId: { in: seatIds },
+          userId,
+        },
+      });
+      return confirmedBooking;
+    });
+
+    return res.json({ payload: { bookingDetails: result } });
+  } catch (err) {
+    console.error("Confirmation error:", err);
+
+    // Provide clear error responses based on business logic failures
+    if (err.message === "BOOKING_NOT_FOUND") {
+      return res.status(404).json({ error: "Booking not found." });
     }
-
-    const FIVE_MINS_MS = 5*60*1000
-    const expireAt = Date.now() + FIVE_MINS_MS
-    try{
-        const existingBooking = await prisma.booking.findMany({
-            where:{
-                showtimeId,
-                status:'CONFIRMED'
-            },select:{
-                seats:true
-            }
-        })
-
-        const arrOfBookedSeats = existingBooking.flatMap(booking=>booking.seats.map(s=>s.id))
-
-        if(arrOfBookedSeats.includes(seatId)){
-            return res.status(409).json({ error: 'Seat is already booked.' });
-        }
-
-        const activeLock = await prisma.seatLock.findFirst({
-            where:{
-                seatId,
-                showtimeId,
-                expiresAt: { gt: new Date() }
-            }
-        })
-        if(activeLock){
-            if(activeLock.userId===userId){
-                await prisma.seatLock.update({
-                    where:{
-                        id:activeLock.id,
-                    },
-                    data:{
-                        expiresAt:new Date(expireAt)
-                    }
-                })
-                return res.status(200).json({message:"Extended time limit for a perticular user."})
-            }else{
-                return res.status(409).json({error:'Seat on hold by other user'})
-            }
-
-        }
-
-        const newLock = await prisma.seatLock.create({
-            data:{
-                seatId,
-                showtimeId,
-                expiresAt:new Date(expireAt),
-                userId
-            }
-        })
-
-        res.status(201).json({message:'Seat upheld',payload:newLock})
-    }catch(err){
-        console.error('Seat locking error:', err);
-        return res.status(500).json({ error: 'Failed to lock seat' });
+    if (err.message === "UNAUTHORIZED_ACCESS") {
+      return res.status(403).json({ error: "You do not own this booking." });
     }
-})
-
-router.delete('/seatLockDelate/:showtimeId/:seatId',async(req,res)=>{
-    const userId = req.userData?.id
-    const {showtimeId, seatId} = req.params
-
-    if(!userId || !showtimeId || !seatId){
-        return res.status(405).json({error:"Invalid Request."})
+    if (err.message === "ALREADY_CONFIRMED") {
+      return res.status(400).json({ error: "Booking is already confirmed." });
     }
 
-    try{
-        await prisma.seatLock.deleteMany({
-            where:{
-                showtimeId,
-                seatId,
-                userId
-            }
-        })
-       return res.status(200).json({ message: 'Seat unlocked successfully' });
-        } catch (error) {
-            return res.status(500).json({ error: 'Failed to unlock seat' });
-        }
-})
+    return res.status(500).json({ error: "Failed to confirm booking." });
+  }
+});
 
-router.delete('/seatLock/cleanup',async(req, res)=>{
-
-     const userId = req.userData?.id
-
-    if(!userId){
-        return res.status(405).json({error:"Invalid Request."})
-    }
-
-    try{
-        await prisma.seatLock.deleteMany({
-            where:{
-                userId
-            }
-        })
-       return res.status(200).json({ message: 'Seat unlocked successfully' });
-        } catch (error) {
-            return res.status(500).json({ error: 'Failed to unlock seat' });
-        }
-
-})
-
-router.post('/reserveBooking/:showtimeId',async(req,res)=>{
-    const {seatIds, ticketPrice, convenienceFee, totalAmount} = req.body 
-    const userId = req.userData?.id
-    const showtimeId = req.params.showtimeId
-
-    if(!seatIds || !ticketPrice || !convenienceFee || !totalAmount || !userId || !showtimeId){
-        return res.status(405).json({error:"Invalid Request."})
-    }
-
-    try{
-        const booking = await prisma.booking.create({
-            data:{
-                userId,
-                ticketPrice,
-                convenienceFee,
-                totalAmount,
-                showtimeId,
-                seats: {
-                    connect: seatIds.map((id:string) => ({ id })),
-                    },
-            },
-            include:{
-                seats:true
-            }
-        })
-
-        if(!booking){
-            return res.status(409).json({error:'Booking not possible'})
-        }
-
-        return res.json({message:'Booking Initiated', payload:{bookingId:booking.id}})
-    }catch(err){
-        console.error('Booking error:', err);
-        return res.status(500).json({ error: 'Failed to lock hold booking' });
-    }
-
-})
-
-router.get('/bookingDetails/:bookingId',async(req,res)=>{
-    const {bookingId} = req.params
-
-    if(!bookingId){
-        return res.status(405).json({error:"Invalid Request."})
-    }
-
-    try{
-        const bookingDetails = await prisma.booking.findUnique({
-            where:{
-                id:bookingId
-            },
-            include:{
-                seats:true,
-                showtime:{
-                    include:{
-                        movie:true,
-                        screen:{
-                            include:{
-                                theater:true
-                            }
-                        }
-                    }
-                },
-                
-            }
-        })
-        if(!bookingDetails){
-             return res.status(409).json({error:'Booking Details not found'})
-        }
-        console.log(bookingDetails)
-        return res.json({
-            payload:{
-                bookingDetails
-            }
-        })
-    }catch(err){
-        console.error('Booking not found:', err);
-        return res.status(500).json({ error: 'Failed to find booking' });
-    }
-})
-
-export default router
+export default router;
